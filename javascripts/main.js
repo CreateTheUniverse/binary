8b5c9f3c824f45cf091d6cb4c10c8796b41f03da
Shay Dwight <shay.dwight+create@gmail.com>
2017-06-23 11:18:51 -0600

    Add files via upload

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,12 +4,21 @@ var bytes = 0;
 var bytesMax = 8;
 var bitsdrives = 0;
 var bitsdriveCost = 8;
+var bytesdrives = 0;
+var bytesdriveCost = 8;
+var byteunlock = false;
 
 function bitsClick(number){
     if(bits >= bitsMax) return;
     bits = bits + number;
     update("bits",bits);
     progBar("bitsbar",bits,bitsMax);
+	if(!byteunlock){
+		if(bits >= 8){
+			fadeIn("byteContainer", 10);
+			byteunlock = true;
+		};
+	};
 };
 
 function bytesClick(number){
@@ -34,6 +43,18 @@ function buybitsDrive(){
     }
 };
 
+function buybytesDrive(){
+    if(bytes >= bytesdriveCost){
+        bytesdrives = bytesdrives + 1;
+        bytes = bytes - bytesdriveCost;
+        bytesMax = bytesMax + 8;
+        update("bytesdrives",bytesdrives);
+        update("bytes",bytes);
+        update("bytesMax",bytesMax);
+        progBar("bytesbar",bytes,bytesMax);
+    }
+};
+
 function progBar(bar,amt,amtMax){
     var myProg = document.getElementById("myProgress");
     var myBar = document.getElementById(bar);
@@ -47,4 +68,21 @@ function progBar(bar,amt,amtMax){
 
 function update(what,number){
     document.getElementById(what).innerHTML = number;
+
+};
+
+function fadeIn(elem, speed) {
+    elem = document.getElementById(elem);
+    if (elem.style.visibility == "hidden") elem.style.visibility = "visible";
+		elem.style.opacity = 1;
+	var total = 100 * speed;
+	var start = performance.now();
+    var fadeCallback = function (timer) {
+		var opacity = (timer - start) / total;
+        elem.style.opacity = opacity;
+        if (!(opacity >= 1)) {
+            requestAnimationFrame(fadeCallback);
+        }
+    };
+	requestAnimationFrame(fadeCallback);
 };
